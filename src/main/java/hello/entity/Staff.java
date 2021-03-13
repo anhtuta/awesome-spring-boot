8d5bbdcdca71f77a67a530ab3e028e2a75e0131b
Anh Tú Tạ <anhtu51195@gmail.com>
2021-03-14 00:05:59 +0700

    Create entiry: store, staff

---
+++ src/main/java/hello/entity/Staff.java
@@ -0,0 +1,46 @@
+package hello.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "staff")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Staff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToOne
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+}
